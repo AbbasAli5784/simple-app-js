585488439cdfb9bdca7c1962500c653acec6d208
Abbas Ali <abbasali5784@gmail.com>
2023-01-26 15:00:47 -0500

    task 1.3

--- scripts.js
+++ scripts.js
@@ -3,3 +3,14 @@ let pokemonList = [
   { name: "Charmander", height: 0.6, type: ["fire", "earth"] },
   { name: "Pikachu", height: 0.4, type: ["lightning", "earth"] },
 ];
+let names = "";
+for (i = 0; i < pokemonList.length; i++) {
+  names +=
+    pokemonList[i].name + " " + " (height: " + pokemonList[i].height + ")";
+
+  if (pokemonList[i].height > 0.6) {
+    names += " -wow thats big! ";
+  }
+  names += "<br>";
+}
+document.write(names);
