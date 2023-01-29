eef294431ad8b860e63604ed68a3a24d8f22c01d
Abbas Ali <abbasali5784@gmail.com>
2023-01-29 13:20:53 -0500

    Wrapped code into IIFE

--- scripts.js
+++ scripts.js
@@ -1,16 +1,37 @@
-let pokemonList = [
-  { name: "Bulbasaur", height: 0.7, type: ["grass", "poison"] },
-  { name: "Charmander", height: 0.6, type: ["fire", "earth"] },
-  { name: "Pikachu", height: 0.4, type: ["lightning", "earth"] },
-];
-let names = "";
-for (i = 0; i < pokemonList.length; i++) {
-  names +=
-    pokemonList[i].name + " " + " (height: " + pokemonList[i].height + ")";
+//old code below!
 
-  if (pokemonList[i].height > 0.6) {
-    names += " -wow thats big! ";
+/*let message = " ";
+pokemonList.forEach(function (user) {
+  if (user.height > 0.6) {
+    message = ":wow thats big!";
+  } else {
+    message = " ";
   }
-  names += "<br>";
-}
-document.write(names);
+  document.write(user.name + " " + user.height + " " + message + "<br>");
+});*/
+let pokemonRepository = (function () {
+  let pokemonList = [
+    { name: "Bulbasaur", height: 0.7, type: ["grass", "poison"] },
+    { name: "Charmander", height: 0.6, type: ["fire", "earth"] },
+    { name: "Pikachu", height: 0.4, type: ["lightning", "earth"] },
+  ];
+  return {
+    add: function (pokemon) {
+      pokemonList.push(pokemon);
+    },
+    getAll: function () {
+      let result = "";
+      pokemonList.forEach(function (pokemon) {
+        let message = "";
+        if (pokemon.height > 0.7) {
+          message = "wow thats big!";
+        }
+        result += pokemon.name + " " + pokemon.height + " " + message + "<br>";
+      });
+      return result;
+    },
+  };
+})();
+pokemonRepository.add({ name: "Squirtle", height: 0.9, type: ["water"] });
+
+document.write(pokemonRepository.getAll());
