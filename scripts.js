cf2d8b5e884c1b1f9947c001f348c9e87bd57100
Abbas Ali <abbasali5784@gmail.com>
2023-02-09 15:49:10 -0500

    struggling to display image height and type

--- scripts.js
+++ scripts.js
@@ -5,6 +5,30 @@ const pokemonRepository = (function () {
     add: function (pokemon) {
       pokemonList.push(pokemon);
     },
+    showModal: function (pokemon) {
+      let modalBody = document.querySelector(".modal-body");
+      let modalTitle = document.querySelector(".modal-title");
+      modalTitle.innerHTML = "";
+      modalBody.innerHTML = "";
+
+      let nameElement = document.createElement("h5");
+      nameElement.innerHTML = pokemon.name;
+
+      let imageElement = document.createElement("img");
+      imageElement.classList.add("modal-img");
+      imageElement.src = pokemon.imageUrl;
+
+      let heightElement = document.createElement("p");
+      heightElement.innerHTML = "height: " + pokemon.height;
+
+      let typesElement = document.createElement("p");
+      typesElement.innerHTML = "types: " + pokemon.types;
+
+      modalTitle.append(nameElement);
+      modalBody.append(imageElement);
+      modalBody.append(heightElement);
+      modalBody.append(typesElement);
+    },
     showDetails: function (pokemon) {
       pokemonRepository.loadDetails(pokemon).then(function () {
         let modal = document.querySelector(".modal");
@@ -30,8 +54,7 @@ const pokemonRepository = (function () {
       button.innerText = pokemon.name;
       button.classList.add("my-button");
       button.addEventListener("click", function () {
-        document.querySelector(".modal-container").classList.add("is-visible");
-        pokemonRepository.showDetails(pokemon);
+        pokemonRepository.showModal(pokemon);
       });
       listItem.appendChild(button);
       list.appendChild(listItem);
